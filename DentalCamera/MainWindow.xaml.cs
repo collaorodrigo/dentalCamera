eace8425330e8f998e457e48ca8040eb961a0f25
FavioHurtado <faviohurtado@focobol.com>
2016-12-28 18:32:25 -0400

    Añadida funcion de guardar imagen

--- DentalCamera/MainWindow.xaml.cs
+++ DentalCamera/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WebEye.Controls.Wpf;
+using Microsoft.Win32;
 
 namespace DentalCamera
 {
@@ -22,32 +23,48 @@ namespace DentalCamera
     public partial class MainWindow : Window
     {
         protected int contador;
+        protected const string ruta = "C:\\temp\\";
+        //protected List<WebCameraId> cameras; 
         public MainWindow()
         {
             InitializeComponent();
             ListaCamara.ItemsSource = Camara1.GetVideoCaptureDevices();
             contador = ListaCamara.Items.Count;
+            //ruta = "C:\\temp\\";
             if (contador != 0)
             {
                 ListaCamara.SelectedItem = ListaCamara.Items[0];
             }
-            else
-            {
-                buttonStart.IsEnabled = false;
-            }
+
+
         }
+
         private void buttonStart_Click(object sender, RoutedEventArgs e)
         {
             //ListaCamara.SelectedItem;
             var cameraId = (WebCameraId)ListaCamara.SelectedItem;
             Camara1.StartCapture(cameraId);
-            buttonCaptura.IsEnabled = true;
+            // buttonCaptura.IsEnabled = true;
+            // buttonStop.IsEnabled = true;
         }
 
         private void buttonCaptura_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("hola");
-            //  BitmapImage image = new BitmapImage(Camara1.GetCurrentImage();
+
+            string ruta1 = System.DateTime.Now.ToString("yyyy-mmm-d-HH-mm-ss");
+            ruta1 = ruta + ruta1;
+            Camara1.GetCurrentImage().Save(ruta1 + ".jpg");
+
+        }
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+            Camara1.StopCapture();
+        }
+
+        private void ListaCamara_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            buttonStart.IsEnabled = e.AddedItems.Count > 0;
         }
     }
 }
